54b7ce288189b6eae97c788f671b764dda35b33b
Aniruddh Iyer <aniruddhiyer1996@gmail.com>
2018-10-28 20:45:18 -0400

    Updates

---
+++ precache-manifest.033df322a370fdcd8b4f6f47f9ac7400.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d86bdae14e2ba2ecc4b0be7297c5a443",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/FoodIcon.d86bdae1.png"
+  },
+  {
+    "revision": "ce525fd083b04802c9ed",
+    "url": "/aniruddi-SSUI-Assignment6/static/css/main.7ab426bf.chunk.css"
+  },
+  {
+    "revision": "cee9e50bd113b797ed21",
+    "url": "/aniruddi-SSUI-Assignment6/static/js/1.cee9e50b.chunk.js"
+  },
+  {
+    "revision": "1a6c11f907b2ef24d2c3",
+    "url": "/aniruddi-SSUI-Assignment6/static/js/runtime~main.1a6c11f9.js"
+  },
+  {
+    "revision": "3c86c05278120eb0cf9a12e64ada80f4",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/Bangalore.3c86c052.jpg"
+  },
+  {
+    "revision": "c33dbf02a6b3742b7d7a8819c7b30765",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/Accenture.c33dbf02.jpg"
+  },
+  {
+    "revision": "bdec6f2250c47f04fbd847617c090805",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/AirportIcon.bdec6f22.png"
+  },
+  {
+    "revision": "0a405d8958dbdef38c73a440dad09007",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/BrigadeMillenium.0a405d89.jpg"
+  },
+  {
+    "revision": "72453732b3ba2067f4a53e139bf448d0",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/ESC.72453732.jpg"
+  },
+  {
+    "revision": "ce525fd083b04802c9ed",
+    "url": "/aniruddi-SSUI-Assignment6/static/js/main.ce525fd0.chunk.js"
+  },
+  {
+    "revision": "768ef03b35d0e7570d3093438d1dfb9a",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/GamingIcon.768ef03b.png"
+  },
+  {
+    "revision": "6d4e169b20cd3eeccd39ae026804cc04",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/HomeIcon.6d4e169b.png"
+  },
+  {
+    "revision": "8433ae706cadce9cd6332e3ec6dc46af",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/InternshipIcon.8433ae70.png"
+  },
+  {
+    "revision": "c247b993ba0e38e5220f91c5d8f1c890",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/KIA.c247b993.jpg"
+  },
+  {
+    "revision": "076613052fc11541fbf413f47e40201b",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/PES.07661305.jpg"
+  },
+  {
+    "revision": "3cb5cba3c13d0cd2e99a8bb169303c0c",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/Social.3cb5cba3.jpg"
+  },
+  {
+    "revision": "93589b121f4f0b878c82df9393c41f09",
+    "url": "/aniruddi-SSUI-Assignment6/static/media/UniversityIcon.93589b12.png"
+  },
+  {
+    "revision": "52ca5a86147249a0a42bf1a149f3b708",
+    "url": "/aniruddi-SSUI-Assignment6/index.html"
+  }
+];
